e6ba12171f2f6af41bd7b63a7b4d0401d325332c
Dominik Decker <dominik.decker97@gmail.com>
2020-03-17 00:38:29 +0100

    Added more tests

--- test/tests.js
+++ test/tests.js
@@ -29,16 +29,17 @@ const invalidNames = [
     '@skip',
     'fragment',
     'on',
+    'aaa...test',
 ];
 
 describe('isValidName', () => {
-    it('correctly checks for valid names', () => {
+    it('checks for valid names', () => {
         validNames.forEach(name => {
             expect(dynql.isValidName(name)).to.equal(true, `"${name}" should have been valid!`);
         });
     });
 
-    it('correctly checks for invalid names', () => {
+    it('checks for invalid names', () => {
         invalidNames.forEach(name => {
             expect(dynql.isValidName(name)).to.equal(false, `"${name}" should have been invalid!`);
         });
@@ -46,7 +47,7 @@ describe('isValidName', () => {
 });
 
 describe('getSpreadFragmentNames', () => {
-    it('correctly finds valid spreaded fragment names', () => {
+    it('finds all valid spreaded fragment names', () => {
         [
             {
                 definition: '{ something { ...test1 } }',
@@ -65,15 +66,126 @@ describe('getSpreadFragmentNames', () => {
                 names: ['test1', 'test2'],
             },
             {
+                definition: '{ something { ... test1... test2 } }',
+                names: ['test1', 'test2'],
+            },
+            {
                 definition: '{ something { ...test.one...test.two } }',
                 names: ['test.one', 'test.two'],
-            }
+            },
+            {
+                definition: '{ something { ...test..one...test.two } }',
+                names: ['test', 'test.two'],
+            },
+            {
+                definition: `{ something { ...test1
+...test2 } }`,
+                names: ['test1', 'test2'],
+            },
         ].forEach(item => {
             const actual = dynql.getSpreadFragmentNames(item.definition);
             expect(actual).to.deep.equal(
                 item.names,
-                `Definition "${item.definition}" gave ["${actual.join('", "')}"], while expecting ["${item.names.join('", "')}"]`
+                `Definition "${item.definition}" did not give proper results!`
             );
         });
     });
+});
+
+describe('getDefinedFragmentNames',() => {
+    it('finds all validly defined fragment names', () => {
+        validNames.forEach(name => {
+            const definition = `fragment ${name} on Something { field }`;
+            const actual = dynql.getDefinedFragmentNames(definition);
+            expect(actual).to.deep.equal(
+                [name],
+                `Definition "${definition}" did not give proper results!`
+            );
+        });
+    });
+
+    it('ignores invalidly defined fragment names', () => {
+        invalidNames.forEach(name => {
+            const definition = `fragment ${name} on Something { field }`;
+            const actual = dynql.getDefinedFragmentNames(definition);
+            expect(actual).to.deep.equal(
+                [],
+                `Definition "${definition}" did not give proper results!`
+            );
+        });
+    });
+});
+
+describe('resolve', () => {
+    it('resolve regular dependencies', () => {
+        const query = '{ something { ...test } }';
+        const store = new dynql.FragmentStore();
+        const fragment = 'fragment test on Something { foobar }';
+        store.registerFragment('test', fragment);
+        expect(store.resolve(query)).to.have.same.members([fragment]);
+    });
+
+    it('resolve nested dependencies', () => {
+        const query = '{ something { ...level1_a ...level1_b ...level1_c } }';
+        const fragments = {
+            level1_a: 'fragment level1_a on foo { ...level2_a }',
+            level1_b: 'fragment level1_b on foo { ...level2_a ...level2_b }',
+            level1_c: 'fragment level1_c on foo { ...level3_a }',
+            level2_a: 'fragment level2_a on foo { ...level3_a }',
+            level2_b: 'fragment level2_b on foo { bar }',
+            level3_a: 'fragment level3_a on foo { test }',
+        };
+        const store = new dynql.FragmentStore();
+        Object.entries(fragments).forEach(([name, fragment]) => {
+            store.registerFragment(name, fragment);
+        });
+        expect(store.resolve(query)).to.have.same.members(Object.values(fragments));
+    });
+
+    it('resolve only required dependencies', () => {
+        const query = '{ something { ...level1_c } }';
+        const requiredFragments = {
+            level1_c: 'fragment level1_c on foo { ...level3_a }',
+            level3_a: 'fragment level3_a on foo { test }',
+        }
+        const fragments = {
+            ...requiredFragments,
+            level1_a: 'fragment level1_a on foo { ...level2_a }',
+            level1_b: 'fragment level1_b on foo { ...level2_a ...level2_b }',
+            level2_a: 'fragment level2_a on foo { ...level3_a }',
+            level2_b: 'fragment level2_b on foo { bar }',
+        };
+        const store = new dynql.FragmentStore();
+        Object.entries(fragments).forEach(([name, fragment]) => {
+            store.registerFragment(name, fragment);
+        });
+        expect(store.resolve(query)).to.have.same.members(Object.values(requiredFragments));
+    });
+
+    it('resolve not yet defined dependencies', () => {
+        const query = '{ something { ...level1_a ...level1_b ...level1_c } } fragment level1_a on bla { test }';
+        const requiredFragments = {
+            level1_b: 'fragment level1_b on foo { ...level2_b }',
+            level1_c: 'fragment level1_c on foo { ...level3_a }',
+            level2_b: 'fragment level2_b on foo { bar }',
+            level3_a: 'fragment level3_a on foo { test }',
+        };
+        const fragments = {
+            ...requiredFragments,
+            level1_a: 'fragment level1_a on foo { ...level2_a }',
+            level2_a: 'fragment level2_a on foo { ...level3_a }',
+        };
+        const store = new dynql.FragmentStore();
+        Object.entries(fragments).forEach(([name, fragment]) => {
+            store.registerFragment(name, fragment);
+        });
+        expect(store.resolve(query)).to.have.same.members(Object.values(requiredFragments));
+    });
+
+    it('throw an error when it can\'t resole a required fragment', () => {
+        const store = new dynql.FragmentStore();
+        expect(() => {
+            store.resolve('{ something { ...foo } }');
+        }).to.throw(dynql.UnresolvedFragmentError);
+    })
 });
